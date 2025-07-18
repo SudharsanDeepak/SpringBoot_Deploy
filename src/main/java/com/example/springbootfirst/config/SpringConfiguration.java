50ad64d7162f9f99f6e902c5a68acce5e6fd558e
SudharsanDeepak <sudharsandeepak12@gmail.com>
2025-07-18 10:39:03 +0530

    new latest

--- src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
+++ src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
@@ -1,30 +1,24 @@
 package com.example.springbootfirst.config;
 
-import com.example.springbootfirst.jwt.JwtAuthenticationFilter;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import com.example.springbootfirst.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -81,7 +75,7 @@ public class SpringConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://employee-management-system-tau-five.vercel.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://employee-management-system-tau-five.vercel.app/","https://springbootems-sudharsandeepaks-projects.vercel.app/","https://springbootems-git-master-sudharsandeepaks-projects.vercel.app/","https://springbootems-1vvz4i2ze-sudharsandeepaks-projects.vercel.app/"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
